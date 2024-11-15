72fb2da0c5dbca4f18ca680b30e68d1352421aa5
mm-110 <massimomontanaro.1993@gmail.com>
2024-11-15 21:39:21 +0100

    Initial Commit

---
+++ DatafileOrganizer/DatafileOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatafileOrganizerApp.swift
+//  DatafileOrganizer
+//
+//  Created by Massimo Montanaro on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DatafileOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DatafileOrganizerTests/DatafileOrganizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DatafileOrganizerTests.swift
+//  DatafileOrganizerTests
+//
+//  Created by Massimo Montanaro on 15/11/24.
+//
+
+import Testing
+@testable import DatafileOrganizer
+
+struct DatafileOrganizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
